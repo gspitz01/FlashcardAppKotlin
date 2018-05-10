184191d286bdc4966750b70f4805848bb74feccd
Greg Spitz <gregspitz6@gmail.com>
2018-05-10 19:33:04 -0400

    Add license and optimize imports for all mock files

--- app/src/androidTestMock/java/com/gregspitz/flashcardappkotlin/addeditflashcard/AddEditFlashcardActivityTest.kt
+++ app/src/androidTestMock/java/com/gregspitz/flashcardappkotlin/addeditflashcard/AddEditFlashcardActivityTest.kt
@@ -1,7 +1,22 @@
+/*
+ * Copyright (C) 2018 Greg Spitz
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.gregspitz.flashcardappkotlin.addeditflashcard
 
 import android.content.Intent
-import android.support.test.InstrumentationRegistry
 import android.support.test.espresso.Espresso.onView
 import android.support.test.espresso.action.ViewActions.click
 import android.support.test.espresso.action.ViewActions.replaceText
@@ -15,9 +30,7 @@ import android.support.test.runner.AndroidJUnit4
 import com.gregspitz.flashcardappkotlin.FlashcardApplication
 import com.gregspitz.flashcardappkotlin.R
 import com.gregspitz.flashcardappkotlin.data.model.Flashcard
-import com.gregspitz.flashcardappkotlin.data.source.FakeFlashcardLocalDataSource
 import com.gregspitz.flashcardappkotlin.data.source.FlashcardDataSource
-import com.gregspitz.flashcardappkotlin.data.source.FlashcardRepository
 import com.gregspitz.flashcardappkotlin.flashcardlist.FlashcardListActivity
 import junit.framework.Assert.assertEquals
 import org.hamcrest.Matchers.not
--- app/src/androidTestMock/java/com/gregspitz/flashcardappkotlin/randomflashcard/RandomFlashcardActivityTest.kt
+++ app/src/androidTestMock/java/com/gregspitz/flashcardappkotlin/randomflashcard/RandomFlashcardActivityTest.kt
@@ -1,8 +1,23 @@
+/*
+ * Copyright (C) 2018 Greg Spitz
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.gregspitz.flashcardappkotlin.randomflashcard
 
 import android.content.Intent
 import android.content.pm.ActivityInfo
-import android.support.test.InstrumentationRegistry
 import android.support.test.espresso.Espresso.onView
 import android.support.test.espresso.action.ViewActions.click
 import android.support.test.espresso.assertion.ViewAssertions.matches
@@ -16,8 +31,6 @@ import android.widget.TextView
 import com.gregspitz.flashcardappkotlin.FlashcardApplication
 import com.gregspitz.flashcardappkotlin.R
 import com.gregspitz.flashcardappkotlin.data.model.Flashcard
-import com.gregspitz.flashcardappkotlin.data.source.FakeFlashcardLocalDataSource
-import com.gregspitz.flashcardappkotlin.data.source.FlashcardRepository
 import org.hamcrest.Description
 import org.hamcrest.Matcher
 import org.junit.Before
