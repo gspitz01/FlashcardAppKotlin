e699b47f3f09cf8541ba81b34ec2ec2e5c1ddbcd
Greg Spitz <gregspitz6@gmail.com>
2018-05-10 19:55:29 -0400

    Fix MainScreenTest so it actually runs a test

--- app/src/androidTestProd/java/com/gregspitz/flashcardappkotlin/MainScreenTest.kt
+++ app/src/androidTestProd/java/com/gregspitz/flashcardappkotlin/MainScreenTest.kt
@@ -16,9 +16,11 @@
 
 package com.gregspitz.flashcardappkotlin
 
+import android.support.test.espresso.Espresso.onView
+import android.support.test.espresso.action.ViewActions.click
+import android.support.test.espresso.assertion.ViewAssertions.matches
 import android.support.test.espresso.matcher.BoundedMatcher
-import android.support.test.espresso.matcher.ViewMatchers.withChild
-import android.support.test.espresso.matcher.ViewMatchers.withText
+import android.support.test.espresso.matcher.ViewMatchers.*
 import android.support.test.rule.ActivityTestRule
 import android.support.test.runner.AndroidJUnit4
 import android.support.v7.widget.RecyclerView
@@ -27,6 +29,7 @@ import com.gregspitz.flashcardappkotlin.data.model.Flashcard
 import org.hamcrest.Description
 import org.hamcrest.Matcher
 import org.junit.Rule
+import org.junit.Test
 import org.junit.runner.RunWith
 
 /**
@@ -42,16 +45,16 @@ class MainScreenTest {
     @Rule @JvmField
     val testRule = ActivityTestRule<MainActivity>(MainActivity::class.java)
 
-//    @Test
-//    fun moveToFlashcardList_containsFlashcards() {
-//        onView(withId(R.id.flashcardListButton)).perform(click())
-//
-//        onView(withText(R.string.app_name)).check(matches(isDisplayed()))
+    @Test
+    fun moveToFlashcardList_containsFlashcards() {
+        onView(withId(R.id.flashcardListButton)).perform(click())
+
+        onView(withText(R.string.app_name)).check(matches(isDisplayed()))
 //        onView(withText(InitialData.flashcards[0].front)).perform(click())
 //
 //        onView(withText(InitialData.flashcards[0].front)).check(matches(isDisplayed()))
 //        onView(withText(InitialData.flashcards[0].back)).check(matches(isDisplayed()))
-//    }
+    }
 
     private fun hasFlashcardFrontForPosition(
             position: Int, flashcard: Flashcard): Matcher<in View>? =
