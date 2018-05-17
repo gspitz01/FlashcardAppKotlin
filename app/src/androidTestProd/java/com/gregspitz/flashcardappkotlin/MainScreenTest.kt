ba868e9b65071670e400d30d00b01c57b4ff79da
Greg Spitz <gregspitz6@gmail.com>
2018-05-16 20:23:00 -0400

    Add MainActivity and update MainScreenTest to use it

--- app/src/androidTestProd/java/com/gregspitz/flashcardappkotlin/MainScreenTest.kt
+++ app/src/androidTestProd/java/com/gregspitz/flashcardappkotlin/MainScreenTest.kt
@@ -17,17 +17,14 @@
 package com.gregspitz.flashcardappkotlin
 
 import android.support.test.espresso.Espresso.onView
-import android.support.test.espresso.action.ViewActions.click
 import android.support.test.espresso.assertion.ViewAssertions.matches
-import android.support.test.espresso.matcher.BoundedMatcher
-import android.support.test.espresso.matcher.ViewMatchers.*
+import android.support.test.espresso.contrib.DrawerActions
+import android.support.test.espresso.contrib.NavigationViewActions.navigateTo
+import android.support.test.espresso.matcher.ViewMatchers.isDisplayed
+import android.support.test.espresso.matcher.ViewMatchers.withId
+import android.support.test.filters.LargeTest
 import android.support.test.rule.ActivityTestRule
 import android.support.test.runner.AndroidJUnit4
-import android.support.v7.widget.RecyclerView
-import android.view.View
-import com.gregspitz.flashcardappkotlin.data.model.Flashcard
-import org.hamcrest.Description
-import org.hamcrest.Matcher
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -36,42 +33,17 @@ import org.junit.runner.RunWith
  * Large test for whole app
  */
 @RunWith(AndroidJUnit4::class)
+@LargeTest
 class MainScreenTest {
-
-    // TODO: make these tests - keeping in mind that this one test failed in travis build #7
-    // for the reason of not having a matching view, whereas on this computer it fails for matching
-    // more than one view.
 
     @Rule @JvmField
     val testRule = ActivityTestRule<MainActivity>(MainActivity::class.java)
 
     @Test
-    fun moveToFlashcardList_containsFlashcards() {
-        onView(withId(R.id.flashcardListButton)).perform(click())
-
-        onView(withText(R.string.app_name)).check(matches(isDisplayed()))
-//        onView(withText(InitialData.flashcards[0].front)).perform(click())
-//
-//        onView(withText(InitialData.flashcards[0].front)).check(matches(isDisplayed()))
-//        onView(withText(InitialData.flashcards[0].back)).check(matches(isDisplayed()))
+    fun moveThroughWholeApp() {
+        onView(withId(R.id.nextFlashcardButton)).check(matches(isDisplayed()))
+        onView(withId(R.id.drawerLayout)).perform(DrawerActions.open())
+        onView(withId(R.id.navDrawer)).perform(navigateTo(R.id.navList))
+        onView(withId(R.id.flashcardRecyclerView)).check(matches(isDisplayed()))
     }
-
-    private fun hasFlashcardFrontForPosition(
-            position: Int, flashcard: Flashcard): Matcher<in View>? =
-            object : BoundedMatcher<View, RecyclerView>(RecyclerView::class.java) {
-                override fun matchesSafely(item: RecyclerView?): Boolean {
-                    if (item == null) {
-                        return false
-                    }
-
-                    val holder = item.findViewHolderForAdapterPosition(position)
-
-                    return holder != null &&
-                            withChild(withText(flashcard.front)).matches(holder.itemView)
-                }
-
-                override fun describeTo(description: Description?) {
-                    description?.appendText("Item has flashcard data at position $position")
-                }
-            }
 }
