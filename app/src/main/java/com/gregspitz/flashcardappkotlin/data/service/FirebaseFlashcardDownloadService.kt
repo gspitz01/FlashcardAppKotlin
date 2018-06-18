c477310a0b8b74a6dbf8429178d627c5705b039b
Greg Spitz <gregspitz6@gmail.com>
2018-06-18 08:20:26 -0400

    Update FirebaseFlashcardDownloadService for newer API
    Also add dependencies for Play Services Authentication and Firebase
    Authentication

--- app/src/main/java/com/gregspitz/flashcardappkotlin/data/service/FirebaseFlashcardDownloadService.kt
+++ app/src/main/java/com/gregspitz/flashcardappkotlin/data/service/FirebaseFlashcardDownloadService.kt
@@ -17,19 +17,22 @@ class FirebaseFlashcardDownloadService : FlashcardDownloadService {
 
     override fun getDownloadCategories(callback: FlashcardDownloadService.GetDownloadCategoriesCallback) {
         categoriesRef.addListenerForSingleValueEvent(object: ValueEventListener {
-            override fun onCancelled(p0: DatabaseError?) {
+            override fun onCancelled(p0: DatabaseError) {
                 callback.onDataNotAvailable()
             }
 
-            override fun onDataChange(p0: DataSnapshot?) {
-                if (p0 != null) {
-                    val downloadCategories = mutableListOf<DownloadCategory>()
-                    for (category in p0.children) {
-                        val cat = category.getValue(DownloadCategory::class.java)
-                        cat?.let {
-                            downloadCategories.add(DownloadCategory(it.name, it.count, category.key))
+            override fun onDataChange(p0: DataSnapshot) {
+                val downloadCategories = mutableListOf<DownloadCategory>()
+                for (category in p0.children) {
+                    val cat = category.getValue(DownloadCategory::class.java)
+                    cat?.let {
+                        if (category.key != null) {
+                            downloadCategories.add(DownloadCategory(it.name, it.count,
+                                    category.key!!))
                         }
                     }
+                }
+                if (downloadCategories.size > 0) {
                     callback.onCategoriesLoaded(downloadCategories)
                 } else {
                     callback.onDataNotAvailable()
@@ -44,25 +47,28 @@ class FirebaseFlashcardDownloadService : FlashcardDownloadService {
 
         flashcardsRef.orderByChild("category_id").equalTo(category.id)
                 .addListenerForSingleValueEvent(object: ValueEventListener {
-            override fun onCancelled(p0: DatabaseError?) {
-                callback.onDataNotAvailable()
-            }
+                    override fun onCancelled(p0: DatabaseError) {
+                        callback.onDataNotAvailable()
+                    }
 
-            override fun onDataChange(p0: DataSnapshot?) {
-                if (p0 != null) {
-                    val flashcards = mutableListOf<DownloadFlashcard>()
-                    for (flashcard in p0.children) {
-                        val card = flashcard.getValue(DownloadFlashcard::class.java)
-                        card?.let {
-                            flashcards.add(DownloadFlashcard(flashcard.key, card.category_name,
-                                    card.category_id, card.front, card.back))
+                    override fun onDataChange(p0: DataSnapshot) {
+                        val flashcards = mutableListOf<DownloadFlashcard>()
+                        for (flashcard in p0.children) {
+                            val card = flashcard.getValue(DownloadFlashcard::class.java)
+                            card?.let {
+                                if (flashcard.key != null) {
+                                    flashcards.add(DownloadFlashcard(flashcard.key!!,
+                                            card.category_name, card.category_id, card.front,
+                                            card.back))
+                                }
+                            }
+                        }
+                        if (flashcards.size > 0) {
+                            callback.onFlashcardsDownloaded(flashcards)
+                        } else {
+                            callback.onDataNotAvailable()
                         }
                     }
-                    callback.onFlashcardsDownloaded(flashcards)
-                } else {
-                    callback.onDataNotAvailable()
-                }
-            }
         })
     }
 }
